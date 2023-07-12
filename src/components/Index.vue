e241d3a57cffa6d90353d3738447d29a5df9bd30
wenzi.luo <wenzi.luo@amh-group.com>
2023-07-13 00:23:13 +0800

    增加路由

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="app">
-    <!-- 路由出口 -->
-    <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
+  <div>
+    <h1>Hello App!</h1>
+  <p>
+    <!-- 使用 router-link 组件来导航. -->
+    <!-- 通过传入 `to` 属性指定链接. -->
+    <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+    <router-link to="/hello">Go to HelloWorld</router-link>
+  </p>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,32 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import VueRouter from 'vue-router'
+import HelloWorld from "./components/HelloWorld.vue";
+import Index from "./components/Index.vue";
 
 Vue.use(ElementUI);
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
+const routes = [
+  { path: '/', component: Index },
+  { path: '/hello', component: HelloWorld },
+]
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
